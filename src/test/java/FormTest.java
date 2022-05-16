e16ae478348b897ece36026faa8a39f0cbba17b3
Moskalenko Elizaveta Ihorivna <elizavetam@zeto.internal>
2022-05-16 15:38:28 +0200

    add basic test

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,3 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.Form;
+import pages.TempMail;
+
 public class FormTest extends BaseTest{
+
+    @Test
+    public void formPositive(){
+        String mail = new TempMail(driver).open().getTempMail();
+        openNewTab();
+        Form form = new Form(driver).open();
+        form.fillFirstName("Jules");
+        form.fillLastName("Doe");
+        form.fillEmail(mail);
+        form.selectRandomSex();
+        form.fillRandomAge(18, 40);
+        form.selectRandomExperience();
+        form.selectRandomProfessions();
+        form.selectRandomContinent();
+        form.selectSeleniumCommands(new String[]{"Switch Commands", "Wait Commands"});
+        form.uploadFile("C:\\Users\\elizavetam\\Documents\\RandomFiles\\SomeFile.txt");
+        form.signIn();
+        Assert.assertTrue(form.isSuccess());
+
+    }
+
 
 }
