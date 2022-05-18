deef15cd86121444727cb31d0b82def62459c9cc
Moskalenko Elizaveta Ihorivna <elizavetam@zeto.internal>
2022-05-18 12:22:56 +0200

    add screenshot capturing

---
+++ src/main/java/utils/Screenshot.java
@@ -0,0 +1,41 @@
+package utils;
+
+import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static utils.config.SCREENSHOTS_DIR;
+
+public class Screenshot {
+    public static void captureScreenshot(ITestResult result) throws IOException {
+        WebDriver driver = (WebDriver)result.getTestContext().getAttribute("WebDriver");
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        String directoryName = result.getInstanceName();
+        String testName = result.getName();
+        Date time = new Date(result.getEndMillis());
+        String destName = getScreenshotDestName(directoryName,testName,time);
+
+        File targetFile = new File(System.getProperty("user.home")+SCREENSHOTS_DIR, destName);
+        FileUtils.copyFile(screenshotFile, targetFile);
+        try (FileInputStream fis = new FileInputStream(targetFile)) {
+            Allure.addAttachment("Screenshot", fis);
+        }
+    }
+
+    public static String getScreenshotDestName(String directoryName, String testName, Date time){
+        SimpleDateFormat formatter = new SimpleDateFormat("yy_MM_dd_HHmmss");
+        String timePart = formatter.format(time);
+        String systemSeparator = System.getProperty("file.separator");
+        return directoryName+systemSeparator+testName+timePart+".png";
+    }
+}
--- src/main/java/utils/config.java
+++ src/main/java/utils/config.java
@@ -6,4 +6,5 @@ public class config {
     public static final DriverManagerType BROWSER = DriverManagerType.CHROME;
     public static final boolean CLEAR_COOKIES_AND_STORAGE = false;
     public static final String SOME_FILE_PATH = "\\Documents\\RandomFiles\\SomeFile.txt";
+    public static final String SCREENSHOTS_DIR = "\\Documents\\AutomationBasicFiles\\Screenshots";
 }
