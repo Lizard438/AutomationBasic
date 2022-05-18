ab0c240d37439bfb68f3854ae098b4b31efb2841
Moskalenko Elizaveta Ihorivna <elizavetam@zeto.internal>
2022-05-18 12:25:29 +0200

    Implement listener onTestFailure functionality

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -8,58 +8,20 @@ import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-import java.io.File;
+import java.io.IOException;
+
+import static utils.Screenshot.captureScreenshot;
 
 public class TestListener extends TestListenerAdapter {
-    WebDriver driver;
-    private static String fileSeperator = System.getProperty("file.separator");
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("***** Error " + result.getName() + " test has failed *****");
-
-        ITestContext context = result.getTestContext();
-        WebDriver driver = (WebDriver)context.getAttribute("driver");
-
-        String testClassName = getTestClassName(result.getInstanceName()).trim();
-
-        String testMethodName = result.getName().toString().trim();
-        String screenShotName = testMethodName + ".png";
-
-        if (driver != null) {
-            String imagePath = ".." + fileSeperator + "Screenshots"
-                    + fileSeperator + "Results" + fileSeperator + testClassName
-                    + fileSeperator
-                    + takeScreenShot(driver, screenShotName, testClassName);
-            System.out.println("Screenshot can be found : " + imagePath);
+        try{
+            captureScreenshot(result);
+        }catch(IOException e){
+            e.printStackTrace();
         }
     }
 
-    public static String takeScreenShot(WebDriver driver,
-                                        String screenShotName, String testName) {
-        try {
-            File file = new File("Screenshots" + fileSeperator + "Results");
-            if (!file.exists()) {
-                System.out.println("File created " + file);
-                file.mkdir();
-            }
-
-            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            File targetFile = new File("Screenshots" + fileSeperator + "Results" + fileSeperator + testName, screenShotName);
-            FileUtils.copyFile(screenshotFile, targetFile);
-
-            return screenShotName;
-        } catch (Exception e) {
-            System.out.println("An exception occured while taking screenshot " + e.getCause());
-            return null;
-        }
-    }
-
-    public String getTestClassName(String testName) {
-        String[] reqTestClassname = testName.split("\\.");
-        int i = reqTestClassname.length - 1;
-        System.out.println("Required Test Name : " + reqTestClassname[i]);
-        return reqTestClassname[i];
-    }
 
 }
