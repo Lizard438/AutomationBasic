fd909ea03d553fd7fb5cf797bf95d909116f871f
Moskalenko Elizaveta Ihorivna <elizavetam@zeto.internal>
2022-05-18 12:27:40 +0200

    fix: change testNg annotations to isolate each test method launch

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import static base.WebDriverFactory.createDriver;
@@ -12,13 +15,16 @@ public class BaseTest {
 
     protected WebDriver driver;
 
-    @BeforeTest
-    public void setUp(){
+    @BeforeMethod
+    public void setUp(ITestContext context){
         driver = createDriver(BROWSER);
         driver.manage().window().maximize();
+        context.setAttribute("WebDriver", driver);
     }
 
-    @AfterTest(alwaysRun = true)
+
+
+    @AfterMethod(alwaysRun = true)
     public void tearDown(){
         if(driver != null){
             if(CLEAR_COOKIES_AND_STORAGE){
@@ -37,4 +43,6 @@ public class BaseTest {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.sessionStorage.clear()");
     }
+
+
 }
