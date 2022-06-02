c073d3371b6d42f9c9299e81d85046a55a4a5aaa
Moskalenko Elizaveta Ihorivna <elizavetam@zeto.internal>
2022-06-02 14:01:04 +0200

    add standart wait

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -1,19 +1,32 @@
 package core;
 
 import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.ProjectConfig;
+
+import java.time.Duration;
 
 public class BasePage {
 
     protected WebDriver driver;
-    protected ProjectConfig cfg = ConfigFactory.create(ProjectConfig.class,System.getProperties());
+    protected ProjectConfig cfg;
+    protected WebDriverWait wait;
 
-    public BasePage(WebDriver driver){
+    protected BasePage(WebDriver driver){
         this.driver = driver;
+        cfg = ConfigFactory.create(ProjectConfig.class,System.getProperties());
+        wait = new WebDriverWait(driver, Duration.ofSeconds(cfg.seconds()));
     }
 
-    public void open(String url){
+    protected void open(String url){
         driver.get(url);
     }
+
+    public WebElement findElement(By element){
+        return wait.until(driver -> driver.findElement(element));
+    }
+
 }
